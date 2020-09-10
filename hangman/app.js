14942cb7b45a453a8c7c4eb0cd6344faa00b2535
Md. Shahriar Parvez <mr.spshuvo@gmail.com>
2020-09-10 08:36:46 +0600

    refactor codes and add readme :tada:

--- hangman/app.js
+++ hangman/app.js
@@ -2,59 +2,14 @@ const puzzleElem = document.querySelector(".game__puzzle");
 const guessElem = document.querySelector(".game__remainingGuess span");
 const gameButton = document.querySelector(".game-overlay__action");
 const gameOverlay = document.querySelector(".game-overlay");
+const keyword = document.querySelector(".keyboard");
+const title = document.querySelector(".game-overlay__title");
+const info = document.querySelector(".game-overlay__info");
+const action = document.querySelector(".game-overlay__action");
 
-let game;
-let word;
-
-const getRandomNumber = async () => {
-  const res = await fetch("https://random-word-api.herokuapp.com/word");
-  const randomWord = await res.json();
-  word = randomWord[0];
-};
-
-getRandomNumber();
-
-const startGame = () => {
-  game = new Hangman(word, 3);
-  puzzleElem.textContent = game.getPuzzle();
-  guessElem.textContent = game.remainingGuess;
-};
-
-const gamePlay = (guessKey) => {
-  game.makeGuess(guessKey);
-  disabledKey(guessKey);
-  puzzleElem.textContent = game.getPuzzle();
-  guessElem.textContent = game.remainingGuess;
-  gameResults(game.status);
-};
-
-const gameResults = (status) => {
-  if (status == "finished" || status === "failed") {
-    setTimeout(() => {
-      gameOverlay.style.display = "flex";
-    }, 300);
-    const title = document.querySelector(".game-overlay__title");
-    const info = document.querySelector(".game-overlay__info");
-    const action = document.querySelector(".game-overlay__action");
-
-    if (status == "finished") {
-      title.textContent = "Congratulation!";
-      info.textContent = "I bet you want to play again.";
-      action.textContent = "Play again!";
-    }
-
-    if (status == "failed") {
-      title.textContent = "Alas! You've Failed.";
-      info.textContent = `The word was ${word}`;
-      action.textContent = "Try again!";
-      gameOverlay.classList.add("failed");
-    }
-  }
-};
-
+// Generating Keyboard Layout
 const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-const keyword = document.querySelector(".keyboard");
 letters.forEach((letter, index) => {
   const element = `<button class="keyboard__letter" onclick="gamePlay(this.dataset.key)" data-key=${letter}>${letter}</button>`;
   keyword.insertAdjacentHTML("beforeend", element);
@@ -63,6 +18,60 @@ letters.forEach((letter, index) => {
   }
 });
 
+// Getting Random Words from API
+let word;
+const getRandomWord = async () => {
+  const res = await fetch("https://random-word-api.herokuapp.com/word");
+  const randomWord = await res.json();
+  word = randomWord[0];
+};
+getRandomWord();
+
+// Create Game Instances
+let game;
+const startGame = () => {
+  console.log(word);
+  game = new Hangman(word, 3);
+  puzzleElem.textContent = game.getPuzzle();
+  guessElem.textContent = game.remainingGuess;
+};
+
+// Game Play Results
+const gameResults = (status) => {
+  if (status == "finished" || status === "failed") {
+    setTimeout(() => {
+      gameOverlay.style.display = "flex";
+    }, 300);
+  }
+  if (status == "finished") {
+    title.textContent = "Congratulation!";
+    info.textContent = "I bet you want to play again.";
+    action.textContent = "Play again!";
+  }
+  if (status == "failed") {
+    title.textContent = "Alas! You've Failed.";
+    info.textContent = `The word was ${word}`;
+    action.textContent = "Try again!";
+    gameOverlay.classList.add("failed");
+  }
+};
+
+// Game Play Functionality
+const gamePlay = (guessKey) => {
+  game.makeGuess(guessKey);
+  disabledKey(guessKey);
+  puzzleElem.textContent = game.getPuzzle();
+  guessElem.textContent = game.remainingGuess;
+  gameResults(game.status);
+};
+
+// Taking Guess Input
+window.addEventListener("keypress", (e) => {
+  const guessKey = String.fromCharCode(e.charCode);
+  gamePlay(guessKey);
+});
+
+// Disabling Guessed Key
 const disabledKey = (guessKey) => {
   const elem = document.querySelector(`[data-key=${guessKey}]`);
   elem.classList.add("keyboard__letter--disabled");
@@ -71,6 +80,7 @@ const disabledKey = (guessKey) => {
 gameButton.addEventListener("click", () => {
   gameOverlay.style.display = "none";
   gameOverlay.classList.remove("failed");
+  getRandomWord();
   resetKeyBoardState();
   startGame();
 });
@@ -79,8 +89,3 @@ const resetKeyBoardState = () => {
   const keys = document.querySelectorAll(".keyboard__letter--disabled");
   keys.forEach((key) => key.classList.remove("keyboard__letter--disabled"));
 };
-
-window.addEventListener("keypress", (e) => {
-  const guessKey = String.fromCharCode(e.charCode);
-  gamePlay(guessKey);
-});
---
+++ hangman/readme.md
@@ -0,0 +1,10 @@
+# 👹 Hangman Game
+
+The popular Hangman Game built with JavaScript using `class` syntax and `fetch()` API.
+For generating random words, I use the [random-word-api](https://random-word-api.herokuapp.com/word).
+
+:pushpin: Rules for the hangman game play can be found [here](<https://en.wikipedia.org/wiki/Hangman_(game)>).
+
+## App Preview 😍
+
+![App Preview](screenshot.gif)
