29671668e66b8ba3e3c19a5a19d5dd76abdddb7c
Md. Shahriar Parvez <mr.spshuvo@gmail.com>
2020-09-11 09:59:15 +0600

    fix minor bugs :bug:

--- hangman/app.js
+++ hangman/app.js
@@ -1,63 +1,77 @@
-const puzzleElem = document.querySelector(".game__puzzle");
-const guessElem = document.querySelector(".game__remainingGuess span");
-const gameButton = document.querySelector(".game-overlay__action");
-const gameOverlay = document.querySelector(".game-overlay");
-const keyword = document.querySelector(".keyboard");
-const title = document.querySelector(".game-overlay__title");
-const info = document.querySelector(".game-overlay__info");
-const action = document.querySelector(".game-overlay__action");
+const puzzleElem = document.querySelector('.game__puzzle');
+const guessElem = document.querySelector('.game__remainingGuess span');
+const gameButton = document.querySelector('.game-overlay__action');
+const gameOverlay = document.querySelector('.game-overlay');
+const keyword = document.querySelector('.keyboard');
+const title = document.querySelector('.game-overlay__title');
+const info = document.querySelector('.game-overlay__info');
+const action = document.querySelector('.game-overlay__action');
 
 // Generating Keyboard Layout
-const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 letters.forEach((letter, index) => {
   const element = `<button class="keyboard__letter" onclick="gamePlay(this.dataset.key)" data-key=${letter}>${letter}</button>`;
-  keyword.insertAdjacentHTML("beforeend", element);
+  keyword.insertAdjacentHTML('beforeend', element);
   if (index == 9 || index == 18) {
-    keyword.insertAdjacentHTML("beforeend", "<br>");
+    keyword.insertAdjacentHTML('beforeend', '<br>');
   }
 });
 
 // Getting Random Words from API
-let word;
-const getRandomWord = async () => {
-  const res = await fetch("https://random-word-api.herokuapp.com/word");
+
+const getRandomWord = async callback => {
+  const res = await fetch('https://random-word-api.herokuapp.com/word');
   const randomWord = await res.json();
-  word = randomWord[0];
+  callback(randomWord[0]);
 };
-getRandomWord();
 
 // Create Game Instances
 let game;
+let randomWord;
 const startGame = () => {
-  console.log(word);
-  game = new Hangman(word, 3);
-  puzzleElem.textContent = game.getPuzzle();
-  guessElem.textContent = game.remainingGuess;
+  getRandomWord(word => {
+    randomWord = word;
+    console.log(word);
+    game = new Hangman(word, 3);
+    puzzleElem.textContent = game.getPuzzle();
+    guessElem.textContent = game.remainingGuess;
+  });
 };
+startGame();
+
+// Start Game Button
+gameButton.addEventListener('click', () => {
+  gameOverlay.style.display = 'none';
+  gameOverlay.classList.remove('failed');
+  resetKeyBoardState();
+  game.status = 'playing';
+});
 
 // Game Play Results
-const gameResults = (status) => {
-  if (status == "finished" || status === "failed") {
+const gameResults = status => {
+  if (status == 'finished' || status === 'failed') {
     setTimeout(() => {
-      gameOverlay.style.display = "flex";
+      gameOverlay.style.display = 'flex';
     }, 300);
   }
-  if (status == "finished") {
-    title.textContent = "Congratulation!";
-    info.textContent = "I bet you want to play again.";
-    action.textContent = "Play again!";
+  if (status == 'finished') {
+    title.textContent = 'Congratulation!';
+    info.textContent = 'I bet you want to play again.';
+    action.textContent = 'Play again!';
+    startGame();
   }
-  if (status == "failed") {
+  if (status == 'failed') {
     title.textContent = "Alas! You've Failed.";
-    info.textContent = `The word was ${word}`;
-    action.textContent = "Try again!";
-    gameOverlay.classList.add("failed");
+    info.textContent = `The word was ${randomWord}`;
+    action.textContent = 'Try again!';
+    gameOverlay.classList.add('failed');
+    startGame();
   }
 };
 
 // Game Play Functionality
-const gamePlay = (guessKey) => {
+const gamePlay = guessKey => {
   game.makeGuess(guessKey);
   disabledKey(guessKey);
   puzzleElem.textContent = game.getPuzzle();
@@ -66,26 +80,21 @@ const gamePlay = (guessKey) => {
 };
 
 // Taking Guess Input
-window.addEventListener("keypress", (e) => {
-  const guessKey = String.fromCharCode(e.charCode);
-  gamePlay(guessKey);
+window.addEventListener('keypress', e => {
+  if (game.status === 'playing') {
+    const guessKey = String.fromCharCode(e.charCode);
+    gamePlay(guessKey);
+  }
 });
 
 // Disabling Guessed Key
-const disabledKey = (guessKey) => {
+const disabledKey = guessKey => {
   const elem = document.querySelector(`[data-key=${guessKey}]`);
-  elem.classList.add("keyboard__letter--disabled");
+  elem.classList.add('keyboard__letter--disabled');
 };
 
-gameButton.addEventListener("click", () => {
-  gameOverlay.style.display = "none";
-  gameOverlay.classList.remove("failed");
-  getRandomWord();
-  resetKeyBoardState();
-  startGame();
-});
-
+// Reset disabled Keys
 const resetKeyBoardState = () => {
-  const keys = document.querySelectorAll(".keyboard__letter--disabled");
-  keys.forEach((key) => key.classList.remove("keyboard__letter--disabled"));
+  const keys = document.querySelectorAll('.keyboard__letter--disabled');
+  keys.forEach(key => key.classList.remove('keyboard__letter--disabled'));
 };
--- hangman/hangman.js
+++ hangman/hangman.js
@@ -1,29 +1,29 @@
 class Hangman {
   constructor(word, remainingGuess) {
-    this.word = word.toLowerCase().split("");
+    this.word = word.toLowerCase().split('');
     this.remainingGuess = remainingGuess;
     this.guessedLetters = [];
-    this.status = "playing";
+    this.status = '';
   }
 
   gameStatus() {
-    const isWin = this.word.every((letter) => this.guessedLetters.includes(letter) || letter == " ");
-    if (!this.remainingGuess) this.status = "failed";
-    else if (isWin) this.status = "finished";
-    else this.status = "playing";
+    const isWin = this.word.every(letter => this.guessedLetters.includes(letter) || letter == ' ');
+    if (!this.remainingGuess) this.status = 'failed';
+    else if (isWin) this.status = 'finished';
+    else this.status = 'playing';
   }
 
   getPuzzle() {
-    let puzzle = "";
-    this.word.forEach((letter) => {
-      if (this.guessedLetters.includes(letter) || letter == " ") puzzle += letter;
-      else puzzle += "_";
+    let puzzle = '';
+    this.word.forEach(letter => {
+      if (this.guessedLetters.includes(letter) || letter == ' ') puzzle += letter;
+      else puzzle += '_';
     });
     return puzzle;
   }
 
   makeGuess(guess) {
-    if (this.status === "finished") return;
+    if (this.status === 'finished') return;
     guess = guess.toLowerCase();
     const isUnique = this.guessedLetters.includes(guess);
     const isCorrect = this.word.includes(guess);
